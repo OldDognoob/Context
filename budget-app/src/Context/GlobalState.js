1abe7962efd1e54748dc675b9468fe769b144941
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-28 20:31:44 +0200

    wip-project

--- budget-app/src/Context/GlobalState.js
+++ budget-app/src/Context/GlobalState.js
@@ -22,6 +22,10 @@ export const GlobalProvider = ({ children }) => {
     dispatch({ type: "DELETE_TRANSACTION", payload: id });
   }
 
+  function addTransaction(transaction) {
+    dispatch({ type: "ADD_TRANSACTION", payload: transaction });
+  }
+
   return (
     <GlobalContext.Provider value={{ transactions: state.transactions, deleteTransaction }}>
       {children}
