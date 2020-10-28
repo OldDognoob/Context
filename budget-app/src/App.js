682203bdabeecd8188d3dca08bf52c7f92662c4b
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-28 18:22:09 +0200

    create budget-balance-component

--- budget-app/src/App.js
+++ budget-app/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Header } from "./Components/Header";
+import { Balance } from "./Components/Balance";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance/>
+      </div>
     </div>
   );
 }
