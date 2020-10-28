c045d291772cd3ef9aca6ed8dbdbf105f1297577
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-28 20:21:28 +0200

    change-dynamic-globalstate

--- budget-app/src/Context/GlobalState.js
+++ budget-app/src/Context/GlobalState.js
@@ -1,5 +1,5 @@
 import React, { createContext, useReducer } from "react";
-import AppReducer from'./AppReducer';
+import AppReducer from "./AppReducer";
 
 //Initial State
 const initialState = {
@@ -17,5 +17,14 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
-  return <GlobalContext.Provider value={{transactions:state.transactions}}>{children}</GlobalContext.Provider>;
+  //Actions
+  function deleteTransaction(id) {
+    dispatch({ type: "DELETE_TRANSACTION", payload: id });
+  }
+
+  return (
+    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+      {children}
+    </GlobalContext.Provider>
+  );
 };
