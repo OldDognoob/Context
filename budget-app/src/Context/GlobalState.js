4c479505f606c31543e64992a8be24f15aaf3c66
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-29 12:23:58 +0200

    wip project

--- budget-app/src/Context/GlobalState.js
+++ budget-app/src/Context/GlobalState.js
@@ -1,34 +1,38 @@
 import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
-//Initial State
+// Initial state
 const initialState = {
-  transactions: [
-    { id: 1, text: "Pasta", amount: -15 },
-    { id: 2, text: "OliveOil", amount: 23 },
-    { id: 3, text: "BlackJacket", amount: 180 },
-    { id: 4, text: "MeatBone", amount: -65 },
-  ],
-};
-//Create context
+  transactions: []
+}
+
+// Create context
 export const GlobalContext = createContext(initialState);
 
-//Provider component
+// Provider component
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
-  //Actions
+  // Actions
   function deleteTransaction(id) {
-    dispatch({ type: "DELETE_TRANSACTION", payload: id });
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    });
   }
 
   function addTransaction(transaction) {
-    dispatch({ type: "ADD_TRANSACTION", payload: transaction });
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    });
   }
 
-  return (
-    <GlobalContext.Provider value={{ transactions: state.transactions, deleteTransaction }}>
-      {children}
-    </GlobalContext.Provider>
-  );
-};
+  return (<GlobalContext.Provider value={{
+    transactions: state.transactions,
+    deleteTransaction,
+    addTransaction
+  }}>
+    {children}
+  </GlobalContext.Provider>);
+}
